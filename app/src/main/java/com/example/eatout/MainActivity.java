969d6b002b5497cd6e998d0a19fc810f9ad1d727
priyanshu <priyanshumaurya2810@gmail.com>
2023-10-08 22:59:38 +0530

    Lab work putting on github

---
+++ app/src/main/java/com/example/eatout/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.eatout;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.CheckBox;
+import android.widget.Button;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private CheckBox pizza,burger,coffee,fries,softdrink;
+    private Button buy;
+    private int totalCost;
+    private String order;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        pizza=(CheckBox) findViewById(R.id.pizza);
+//        pizza.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view) {
+//                pizza.setChecked(!pizza.isChecked());
+//            }
+//        });
+
+        burger=(CheckBox) findViewById(R.id.burger);
+//        burger.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view) {
+//                burger.setChecked(!burger.isChecked());
+//            }
+//        });
+        coffee=(CheckBox) findViewById(R.id.coffee);
+//        coffee.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view) {
+//                coffee.setChecked(!coffee.isChecked());
+//            }
+//        });
+
+        fries=(CheckBox) findViewById(R.id.fries);
+//        fries.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view) {
+//                fries.setChecked(!fries.isChecked());
+//            }
+//        });
+        softdrink=(CheckBox) findViewById(R.id.softdrink);
+//        softdrink.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view) {
+//                softdrink.setChecked(!softdrink.isChecked());
+//            }
+//        });
+
+//        pizza - 199/-
+//        Burger - 99/-
+//        Coffee - 79/-
+//        Fries - 99/-
+//        Soft Drink - 50/-
+
+        buy=(Button) findViewById(R.id.buy);
+        buy.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                totalCost=0;
+                order="";
+                if(pizza.isChecked()){
+                    totalCost+=199;
+                    order+="Pizza(+199)\n";
+                }if(burger.isChecked()){
+                    totalCost+=99;
+                    order+="Burger(+99)\n";
+                }if(coffee.isChecked()){
+                    totalCost+=79;
+                    order+="Coffee(+79)\n";
+                }if(fries.isChecked()){
+                    totalCost+=99;
+                    order+="Fries(+99)\n";
+                }if(softdrink.isChecked()){
+                    totalCost+=50;
+                    order+="Soft Drink(+50)\n";
+                }
+                order=order.concat("Total Cost : ");
+                order=order.concat(Integer.toString(totalCost));
+
+                CharSequence text = order;
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(getApplicationContext(), text, duration);
+                toast.show();
+            }
+        });
+
+    }
+}
